3210fcbb6b829c8f2967fea307d7b6e76e96d119
andrew-marquezin <andrewmmarinho@gmail.com>
2023-07-06 13:23:10 -0300

    requisito 1 finalizado

--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -13,4 +13,40 @@ describe('Teste o componente App', () => {
     expect(aboutLink).toBeInTheDocument();
     expect(favoriteLink).toBeInTheDocument();
   });
+
+  it('Teste se ao clicar no link Home, a rota / é renderizada', async () => {
+    const screen = renderWithRouter(<App />, { route: '/' });
+    const { user } = screen;
+
+    const homeLink = screen.getByRole('link', { name: 'Home' });
+    await user.click(homeLink);
+
+    expect(screen.getByText(/Encountered Pokémon/i)).toBeInTheDocument();
+  });
+
+  it('Teste se ao clicar no link About, a rota /about é renderizada', async () => {
+    const screen = renderWithRouter(<App />, { route: '/' });
+    const { user } = screen;
+
+    const aboutLink = screen.getByRole('link', { name: 'About' });
+    await user.click(aboutLink);
+
+    expect(screen.getByText(/About Pokédex/i)).toBeInTheDocument();
+  });
+
+  it('Teste se ao clicar no link Favorite Pokémon, a rota /favorites é renderizada', async () => {
+    const screen = renderWithRouter(<App />, { route: '/' });
+    const { user } = screen;
+
+    const favoriteLink = screen.getByRole('link', { name: 'Favorite Pokémon' });
+    await user.click(favoriteLink);
+
+    expect(screen.getByRole('heading', { name: /Favorite Pokémon/i })).toBeInTheDocument();
+  });
+
+  it('Teste se ao entrar em uma rota inexistente, a rota not found é renderizada', () => {
+    const screen = renderWithRouter(<App />, { route: '/inexistente' });
+
+    expect(screen.getByText(/Page requested not found/i)).toBeInTheDocument();
+  });
 });
