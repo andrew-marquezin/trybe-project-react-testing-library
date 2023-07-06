8b48eb7c3c623897d01591b5ecd6dc7774839a6f
andrew-marquezin <andrewmmarinho@gmail.com>
2023-07-06 12:41:43 -0300

    primeira parte do requisito 1

--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -1,1 +1,16 @@
-test('', () => {});
+import App from '../App';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Teste o componente App', () => {
+  it('Teste se o topo da aplicação contém um conjunto fixo de links de navegação', () => {
+    const screen = renderWithRouter(<App />, { route: '/' });
+
+    const homeLink = screen.getByRole('link', { name: 'Home' });
+    const aboutLink = screen.getByRole('link', { name: 'About' });
+    const favoriteLink = screen.getByRole('link', { name: 'Favorite Pokémon' });
+
+    expect(homeLink).toBeInTheDocument();
+    expect(aboutLink).toBeInTheDocument();
+    expect(favoriteLink).toBeInTheDocument();
+  });
+});
